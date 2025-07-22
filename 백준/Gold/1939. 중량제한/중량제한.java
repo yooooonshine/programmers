7275c288c9dab5d2155cb738dbe00886fe82cb27
YOON CHAN HO <108210104+yooooonshine@users.noreply.github.com>
2025-07-22 11:13:07 +0900

    [Gold III] Title: 중량제한, Time: 588 ms, Memory: 57744 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,106 +1,101 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
+	public static int N;
+	public static int M;
 	public static List<List<Edge>> adj;
-	public static int END;
-	public static long MAX = 0;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken()); // 섬의 수
-		int M = Integer.parseInt(st.nextToken()); // 다리 수
+		N = Integer.parseInt(st.nextToken()); // 섬
+		M = Integer.parseInt(st.nextToken()); // 다리수
 
 		adj = new ArrayList<>();
 		for (int i = 0; i <= N; i++) {
 			adj.add(new ArrayList<>());
 		}
 
-		long max = 0;
-		for (int i = 1; i <= M; i++) {
+		for (int m = 1; m <= M; m++) {
 			st = new StringTokenizer(br.readLine());
 
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			long c = Long.parseLong(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
 
-			adj.get(a).add(new Edge(b, c));
-			adj.get(b).add(new Edge(a, c));
-
-			if (max < c) {
-				max = c;
-			}
+			adj.get(a).add(new Edge(b, d));
+			adj.get(b).add(new Edge(a, d));
 		}
 
 		st = new StringTokenizer(br.readLine());
-		int START = Integer.parseInt(st.nextToken());
-		END = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken()); // 시작 섬
+		int e = Integer.parseInt(st.nextToken()); // 도착 섬
 
-
-		long s = 1;
-		long e = max;
-
-		long result = 0;
-		while (s <= e) {
-			long mid = (s + e) / 2;
-
-			if (dfs(START, N, mid)) {
-				s = mid + 1;
-
-				result = mid;
-			} else {
-				e = mid - 1;
-			}
-		}
-
-		System.out.println(result);
+		System.out.println(djk(s, e));
 	}
 
-	public static boolean dfs(int s, int N, long mid) {
+	public static int djk(int s, int e) {
 		boolean[] visit = new boolean[N + 1];
+		int[] dist = new int[N + 1];
 
-		Stack<Integer> stack = new Stack<>();
-		stack.add(s);
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.add(new Edge(s, 2000000000));
 
-		while (!stack.isEmpty()) {
-			int cur = stack.pop();
+		while(!pq.isEmpty()) {
+			Edge now = pq.poll();
+			int nowE = now.e;
+			int nowD = now.d;
 
-			if (cur == END) {
-				return true;
+			if (visit[nowE]) {
+				continue;
 			}
+			visit[nowE] = true;
 
-			for (Edge e : adj.get(cur)) {
-				if (!visit[e.e] && e.w >= mid) {
-					visit[e.e] = true;
-					stack.add(e.e);
+			for (Edge next : adj.get(nowE)) {
+				int nextD = next.d;
+				int nextE = next.e;
+
+
+				if (dist[nextE] < Math.max(dist[nextE], Math.min(nowD, nextD))) {
+					dist[nextE] = Math.max(dist[nextE], Math.min(nowD, nextD));
+					pq.add(new Edge(nextE, dist[nextE]));
 				}
 			}
+
 		}
 
-		return false;
+		return dist[e];
 	}
 
-	public static class Edge {
+	public static class Edge implements Comparable<Edge> {
 		int e;
-		long w;
+		int d;
 
-		public Edge(int e, long w) {
+		public Edge(int e, int d) {
 			this.e = e;
-			this.w = w;
+			this.d = d;
+		}
+
+		@Override
+		public int compareTo(Edge e) {
+			return e.d - this.d; // 내림차순
 		}
 	}
 }
 
-// N 개의 섬
-// 다리 중량  제한
 
-// N, M(섬 개수, 개의 줄)
-// M 개의 줄
-// A, B, C (A섬, B섬 사이 중량C인 다리)
-// 공장 위치 섬의 번호 두 개
+// 이분탐색
 
-// 두 섬 사이 여러 개의  다리 가능, 양방향 -> 그래프네
+// 모든 경로에서 최대?
 
+// 이분탐색
+// 다익스트라
+// 큐?
+// 인접리스트 최소값
+// 큐에 추가
+// 최대값을 갱신하는 경우에 추가
+// 모든 경로는 0으로 시작.
