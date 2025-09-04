bb4aff077bd6665d67db21f27cc6bc066c94f3e2
YOON CHAN HO <108210104+yooooonshine@users.noreply.github.com>
2025-09-04 13:42:36 +0900

    [Gold V] Title: 배, Time: 384 ms, Memory: 16260 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -2,46 +2,68 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Integer[] cranes = new Integer[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) cranes[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(cranes, Collections.reverseOrder());
 
-        int M = Integer.parseInt(br.readLine());
-        Integer[] boxes = new Integer[M];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) boxes[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(boxes, Collections.reverseOrder());
 
-        // 불가능한 경우
-        if (boxes[0] > cranes[0]) {
-            System.out.println(-1);
-            return;
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int count = 0;
-        boolean[] visited = new boolean[M];
-        int moved = 0; // 옮긴 박스 수
+		int N = Integer.parseInt(br.readLine());
+		Integer[] crains = new Integer[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int n = 0; n < N; n++) {
+			crains[n] = Integer.parseInt(st.nextToken());
+		}
 
-        while (moved < M) {
-            count++;
-            int boxIdx = 0;
-            for (int i = 0; i < N; i++) { // 각 크레인
-                while (boxIdx < M) {
-                    if (!visited[boxIdx] && cranes[i] >= boxes[boxIdx]) {
-                        visited[boxIdx] = true;
-                        moved++;
-                        boxIdx++;
-                        break;
-                    }
-                    boxIdx++;
-                }
-            }
-        }
+		Arrays.sort(crains, Collections.reverseOrder());
 
-        System.out.println(count);
-    }
+		int M = Integer.parseInt(br.readLine()); // 박스의 수
+		Integer[] boxes = new Integer[M];
+		st = new StringTokenizer(br.readLine());
+		for (int m = 0; m < M; m++) {
+			boxes[m] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(boxes, Collections.reverseOrder());
+
+		// 만약 박스를 못 옮기면 -1
+		if (crains[0] < boxes[0]) {
+			System.out.println(-1 + "");
+			return;
+		}
+
+		boolean[] visit = new boolean[M];
+		int moveCount = 0;
+		int result = 0;
+
+		while(moveCount < M) {
+			result++;
+			int boxIndex = 0;
+
+			for (int n = 0; n < N; n++) {
+				while (boxIndex < M) {
+					if (!visit[boxIndex] && crains[n] >= boxes[boxIndex]) {
+						visit[boxIndex] = true;
+						moveCount++;
+						boxIndex++;
+						break;
+					}
+					boxIndex++;
+				}
+			}
+		}
+
+
+		System.out.println(result);
+	}
 }
+
+
+// 그리디인가
+// dp는 아니야
+// 일단 정렬부터 해야할거같아
+
+// 32 28 25 23
+// 32 27 24 20 18 16 15 10 7 5
+
+// 단순 PQ다. PQ에서 하나씩 빼면서 채운다.
+// 크기가 맞지 않는다면 이건 잠시 두고, 나중에 다시 PQ에 넣는다.
